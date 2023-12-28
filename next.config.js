15666cdf9a057f055fedc02391bcd0da775b8c11
Kei <n@iiv.jp>
2023-12-28 14:27:49 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: "/betalogg",
+  basePath: "/betalogg",
+  output: 'export'
+};
+
+module.exports = nextConfig;
