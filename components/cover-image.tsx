52eadada6231b2539dcace687f7d1f39f96fb662
Kei <n@iiv.jp>
2023-12-28 16:45:00 +0900

    change image path 2.

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -11,7 +11,7 @@ type Props = {
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <Image
-      src={'/betalogg/' + src}
+      src={'/betalogg' + src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm w-full', {
         'hover:shadow-lg transition-shadow duration-200': slug,
