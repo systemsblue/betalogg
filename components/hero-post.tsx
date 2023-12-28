124027397fda3b91bbdf62b19f7fe2020c03b7d6
Kei <n@iiv.jp>
2023-12-28 14:06:21 +0900

    first commit

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -28,7 +28,7 @@ const HeroPost = ({
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+          <h3 className="mb-4 text-3xl lg:text-5xl leading-tight">
             <Link
               as={`/posts/${slug}`}
               href="/posts/[slug]"
@@ -37,13 +37,7 @@ const HeroPost = ({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+          - Betalogg
         </div>
       </div>
     </section>
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -12,6 +12,9 @@ import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import type PostType from '../../interfaces/post'
 
+import sysb from '../../public/assets/blog/systemsblue_s4.png'
+import CoverImage from '../../components/cover-image'
+
 type Props = {
   post: PostType
   morePosts: PostType[]
@@ -35,7 +38,6 @@ export default function Post({ post, morePosts, preview }: Props) {
             <article className="mb-32">
               <Head>
                 <title>{title}</title>
-                <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
                 title={post.title}
@@ -44,6 +46,9 @@ export default function Post({ post, morePosts, preview }: Props) {
                 author={post.author}
               />
               <PostBody content={post.content} />
+              <div>
+                <center><a href="http://systemsblue.iiv.jp"><img src="/assets/blog/systemsblue_s4.png" /></a></center>
+              </div>
             </article>
           </>
         )}
